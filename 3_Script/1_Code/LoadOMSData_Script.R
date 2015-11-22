ddd17d6dcb06d63f2d63d3dcf0bab745208c172e
sinhn <sinhnhn@live.com>
2015-11-22 10:37:15 +0700

    Update script capital letter

--- 3_Script/1_Code/LoadOMSData_Script.R
+++ 3_Script/1_Code/LoadOMSData_Script.R
@@ -13,27 +13,26 @@ addHandler(writeToFile, logger="IDInvoiceCheck",
                           paste0("OMSLoading_",dateReport,".csv")))
 addHandler(writeToConsole , logger="IDInvoiceCheck.Log")
 tryCatch({
-loginfo("Initial Setup", logger = "IDInvoiceCheck.Log")
-source("3_Script/1_Code/fn_LoadInvoiceData.R")
-source("3_Script/1_Code/fn_GeneratePackageData.R")
-source("3_Script/1_Code/fn_LoadOMSData.R")
-
-OMSDataFolder <- file.path("1_Input/OMS_Data")
-
-loginfo("Start Loading OMS Data", logger = "IDInvoiceCheck.Log")
-OMSData <- LoadOMSData(OMSDataFolder)
-cat("\r\n")
-loginfo("Done Loading OMS Data", logger = "IDInvoiceCheck.Log")
-
-loginfo("Start Summarize Package Data", logger = "IDInvoiceCheck.Log")
-PackageDataSummarized <- GeneratePackageData(OMSData)
-loginfo("Done Package Summary Data", logger = "IDInvoiceCheck.Log")
-
-loginfo("Start Saving RData Files", logger = "IDInvoiceCheck.Log")
-save(OMSData, file = "3_Script/3_RData/OMSData.RData")
-save(PackageDataSummarized, file = "3_Script/3_RData/PackageDataSummarized.RData")
-loginfo("Done Preparing OMS Data", logger = "IDInvoiceCheck.Log")
-
+  loginfo("Initial Setup", logger = "IDInvoiceCheck.Log")
+  source("3_Script/1_Code/fn_loadOMSData.R")
+  source("3_Script/1_Code/fn_GeneratePackageData.R")
+  
+  OMSDataFolder <- file.path("1_Input/OMS_Data")
+  
+  loginfo("Start Loading OMS Data", logger = "IDInvoiceCheck.Log")
+  OMSData <- LoadOMSData(OMSDataFolder)
+  cat("\r\n")
+  loginfo("Done Loading OMS Data", logger = "IDInvoiceCheck.Log")
+  
+  loginfo("Start Summarize Package Data", logger = "IDInvoiceCheck.Log")
+  PackageDataSummarized <- GeneratePackageData(OMSData)
+  loginfo("Done Package Summary Data", logger = "IDInvoiceCheck.Log")
+  
+  loginfo("Start Saving RData Files", logger = "IDInvoiceCheck.Log")
+  save(OMSData, file = "3_Script/3_RData/OMSData.RData")
+  save(PackageDataSummarized, file = "3_Script/3_RData/PackageDataSummarized.RData")
+  loginfo("Done Preparing OMS Data", logger = "IDInvoiceCheck.Log")
+  
 },error = function(err){
   logerror(err, logger = "IDInvoiceCheck")
   logerror("PLease send 3_Script/Log folder to Regional OPS BI for additional support",
